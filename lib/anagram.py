717a35d9b25c8a4705952a495b14b0006002f3b8
Kat Tannehill <katetannehill@gmail.com>
2023-10-25 15:19:54 -0600

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+
+    # instantiates with single arg, word
+    # method match()
+    # returns empty list if no anagrams
+    # returns a list of words with matching letters
+
+    def __init__(self, poss_anagram):
+        self.poss_anagram = poss_anagram
+    
+    def match(self, list):
+        matches = []
+        for word in list:
+            if sorted([char for char in self.poss_anagram]) == sorted([char for char in word]):
+                matches.append(word)
+        return matches
